b4a44572e2dc68628f04cb7bbf9b609b8ba4d701
Patrick Murray <patrick@patmurraydev.com>
2016-09-10 07:03:21 -0400

    Added map for first screen and image to second (re did layout some)

--- PennApps2016f/PennApps2016f/PHWelcomeViewController.swift
+++ PennApps2016f/PennApps2016f/PHWelcomeViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class PHWelcomeViewController: UIViewController {
+class PHWelcomeViewController: UIViewController, CLLocationManagerDelegate {
     
     
     @IBOutlet weak var topLabel: UILabel!
@@ -17,16 +19,61 @@ class PHWelcomeViewController: UIViewController {
     @IBOutlet weak var button2: HPButton!
     @IBOutlet weak var button3: UIButton!
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        manager.delegate = self
         // Do any additional setup after loading the view.
+        zoomToRegion()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func zoomToRegion() {
+        
+        let region = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2D(latitude: 39.995168, longitude: -75.149842), 5928*2, 5928*2)
+        mapView.setRegion(region, animated: true)
+        
+        manager.requestLocation()
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.requestWhenInUseAuthorization()
+
+        
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Found user's location: \(location)")
+            
+            let toLocation: CLLocation = CLLocation(latitude: 39.995168, longitude: -75.149842)
+            let fromLocation: CLLocation = location
+            
+            
+            let distance = fromLocation.distance(from: toLocation)
+            // 5928
+            
+            
+            let region = MKCoordinateRegionMakeWithDistance(location.coordinate, distance*2, distance*2)
+            
+            print("\(distance)")
+            
+            mapView.setRegion(region, animated: true)
+
+            
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
     }
     
 
