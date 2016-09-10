0d23dc0d9fa8f9a76a9e27785c56dac07b8a8641
Patrick Murray <patrick@patmurraydev.com>
2016-09-10 00:51:07 -0400

    Added location tracking (non continuous) -- needs lots of improvement

--- PennApps2016f/PennApps2016f/AppDelegate.swift
+++ PennApps2016f/PennApps2016f/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
+    let manager = CLLocationManager()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        manager.delegate = self
+
+        
         return true
     }
 
@@ -35,7 +40,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        manager.requestLocation()
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.requestWhenInUseAuthorization()
+        
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Found user's location: \(location)")
+            
+            let toLocation: CLLocation = CLLocation(latitude: 39.995168, longitude: -75.149842)
+            let fromLocation: CLLocation = location
+            
+            
+            let distance = fromLocation.distance(from: toLocation)
+            print("DISTANCE: \(distance)")
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
--- PennApps2016f/PennApps2016f/PHWelcomeViewController.swift
+++ PennApps2016f/PennApps2016f/PHWelcomeViewController.swift
@@ -31,9 +31,30 @@ class PHWelcomeViewController: UIViewController {
     
 
     @IBAction func button1Tapped(_ sender: AnyObject) {
+        
+        // lyft://ridetype?id=lyft&pickup[latitude]=37.764728&pickup[longitude]=-122.422999&destination[latitude]=37.7763592&destination[longitude]=-122.4242038
+        
+        
+        // If Lyft is not installed, send the user to the Apple App Store
+        let myApp = UIApplication.shared
+        let lyftAppURL = NSURL(string: "lyft://ridetype?id=lyft_line&destination[latitude]=39.995168&destination[longitude]=-75.149842")!
+        if myApp.canOpenURL(lyftAppURL as URL) {
+            // Lyft is installed; launch it
+            myApp.openURL(lyftAppURL as URL)
+        } else {
+            // Lyft not installed; open App Store
+            let lyftAppStoreURL = NSURL(string: "https://itunes.apple.com/us/app/lyft-taxi-bus-app-alternative/id529379082")!
+            myApp.openURL(lyftAppStoreURL as URL)
+        }
+        
     }
     
     @IBAction func button2Tapped(_ sender: AnyObject) {
+        
+        
+        self.callNumber(phoneNumber: "19732948935")
+        
+        
     }
     
     @IBAction func button3Tapped(_ sender: AnyObject) {
@@ -43,7 +64,14 @@ class PHWelcomeViewController: UIViewController {
     
     
     
-    
+    private func callNumber(phoneNumber:String) {
+        if let phoneCallURL:NSURL = NSURL(string: "tel://\(phoneNumber)") {
+            let application:UIApplication = UIApplication.shared
+            if (application.canOpenURL(phoneCallURL as URL)) {
+                application.openURL(phoneCallURL as URL);
+            }
+        }
+    }
     
     
     
