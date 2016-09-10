c05afd3b67cf66b7548c3f1f2840abdf44415eaa
Patrick Murray <patrick@patmurraydev.com>
2016-09-09 23:51:48 -0400

    Added ViewControllers for first two screens

---
+++ PennApps2016f/PHCheckinViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PHCheckinViewController.swift
+//  PennApps2016f
+//
+//  Created by Patrick Murray on 9/09/2016.
+//  Copyright © 2016 Patrick Murray. All rights reserved.
+//
+
+import UIKit
+
+class PHCheckinViewController: UIViewController {
+    
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var button1: HPButton!
+    @IBOutlet weak var button2: HPButton!
+    @IBOutlet weak var button3: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func button1Tapped(_ sender: AnyObject) {
+    }
+
+    @IBAction func button2Tapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func button3Tapped(_ sender: AnyObject) {
+    }
+    
+    
+
+}
---
+++ PennApps2016f/PennApps2016f/PHWelcomeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PHWelcomeViewController.swift
+//  PennApps2016f
+//
+//  Created by Patrick Murray on 9/09/2016.
+//  Copyright © 2016 Patrick Murray. All rights reserved.
+//
+
+import UIKit
+
+class PHWelcomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var button1: HPButton!
+    @IBOutlet weak var button2: HPButton!
+    @IBOutlet weak var button3: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func button1Tapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func button2Tapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func button3Tapped(_ sender: AnyObject) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
